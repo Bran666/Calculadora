d838a3f1620a91dcaedda7fb915fa7f4e9f636f9
KyRa.666 <119646986+KyRA-666@users.noreply.github.com>
2023-03-19 23:05:14 -0500

    Renovacion De la Calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,19 +1,42 @@
-let valor1 = document.querySelector("#valor1").value;
-let valor2 = document.querySelector("#valor2").value;
+let valor1 = document.querySelector("#valor1")
+let valor2 = document.querySelector("#valor2")
 let resultado = document.querySelector("#resultado");
 
-let btnSuma = document.querySelector("#suma");
-let btnResta = document.querySelector("#resta");
-let btnMultiplicar = document.querySelector("#multiplicacion");
-let btnDividir = document.querySelector("#division");
+let btnSuma = document.querySelector("#sumar");
+let btnResta = document.querySelector("#restar");
+let btnMultiplicar = document.querySelector("#multiplicar");
+let btnDividir = document.querySelector("#dividir");
 
 
-btnSuma.addEventListener("click",suma)
+btnSuma.addEventListener("click",sumacion)
 
-
-function suma (){
+function sumacion (){
     let v1 = valor1.value;
     let v2 = valor2.value;
-    let suma = v1+v2;
+    let suma = Number(v1)+Number(v2);
     resultado.innerHTML = suma;
+}
+
+btnResta.addEventListener("click",restacion)
+function restacion (){
+    let v1 = valor1.value;
+    let v2 = valor2.value;
+    let resta = v1-v2;
+    resultado.innerHTML = resta;
+}
+
+btnDividir.addEventListener("click",dividicion)
+function dividicion (){
+    let v1 = valor1.value;
+    let v2 = valor2.value;
+    let resta = v1/v2;
+    resultado.innerHTML = resta;
+}
+
+btnMultiplicar.addEventListener("click",multiplicacion)
+function multiplicacion (){
+    let v1 = valor1.value;
+    let v2 = valor2.value;
+    let resta = v1*v2;
+    resultado.innerHTML = resta;
 }
